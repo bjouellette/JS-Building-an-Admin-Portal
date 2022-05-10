8bfdcd4698e75867eafbb54ac6b45ad525c3f89d
bjouellette <bjouellette1@gmail.com>
2022-05-10 18:30:46 -0500

    added files

--- server.js
+++ server.js
@@ -19,24 +19,67 @@ async function main() {
 
     app.patch('/updateBook', async (req, res) => {
         let books = await loadBooks()
-        if (!req.body.id) return res.status(400).json({ error: true, message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
+        if (!req.body.id) return res.status(400).json({
+            error: true,
+            message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`
+        })
         let book = books.find(book => book.id === req.body.id);
-        if (!book) return res.status(404).json({ error: true, message: `Could not find a book with an id of ${req.body.id}` })
-        const { title, year, quantity, imageURL, description } = { ...book, ...req.body };
-        Object.assign(book, { title, year, quantity, imageURL, description });
+        if (!book) return res.status(404).json({
+            error: true,
+            message: `Could not find a book with an id of ${req.body.id}`
+        })
+        const {
+            title,
+            year,
+            quantity,
+            imageURL,
+            description
+        } = {
+            ...book,
+            ...req.body
+        };
+        Object.assign(book, {
+            title,
+            year,
+            quantity,
+            imageURL,
+            description
+        });
         await saveBooks(books)
         res.json(book)
     })
 
     app.post('/addBook', async (req, res) => {
         let books = await loadBooks()
-        if (!req.body.title) return res.status(400).json({ error: true, message: `'title' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
-        if (!req.body.quantity) return res.status(400).json({ error: true, message: `'quantity' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
-        if (!req.body.description) return res.status(400).json({ error: true, message: `'description' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
+        if (!req.body.title) return res.status(400).json({
+            error: true,
+            message: `'title' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`
+        })
+        if (!req.body.quantity) return res.status(400).json({
+            error: true,
+            message: `'quantity' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`
+        })
+        if (!req.body.description) return res.status(400).json({
+            error: true,
+            message: `'description' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`
+        })
 
-        const { title, year, quantity, imageURL, description } = req.body;
+        const {
+            title,
+            year,
+            quantity,
+            imageURL,
+            description
+        } = req.body;
         const id = books.reduce((id, book) => Math.max(book.id + 1, id), 1);
-        const book = { id, title, year, quantity, imageURL, description }
+        const book = {
+            id,
+            title,
+            year,
+            quantity,
+            imageURL,
+            description
+        }
         books.push(book);
         await saveBooks(books)
         res.json(book)
@@ -44,9 +87,15 @@ async function main() {
 
     app.delete('/removeBook/:id', async (req, res) => {
         let books = await loadBooks()
-        if (!req.params.id) return res.status(400).json({ error: true, message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
+        if (!req.params.id) return res.status(400).json({
+            error: true,
+            message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.`
+        })
         let bookToDelete = books.find(book => book.id === parseInt(req.params.id));
-        if (!bookToDelete) return res.status(404).json({ error: true, message: `Could not find a book with an id of ${req.body.id}` })
+        if (!bookToDelete) return res.status(404).json({
+            error: true,
+            message: `Could not find a book with an id of ${req.body.id}`
+        })
         books = books.filter(book => book !== bookToDelete);
         await saveBooks(books)
         res.json(bookToDelete)
@@ -64,12 +113,16 @@ async function main() {
 const DB_PATH = Path.join(__dirname, 'db.json')
 
 async function loadBooks() {
-    let { books } = JSON.parse(await Fs.readFile(DB_PATH))
+    let {
+        books
+    } = JSON.parse(await Fs.readFile(DB_PATH))
     return books
 }
 
 async function saveBooks(books) {
-    await Fs.writeFile(DB_PATH, JSON.stringify({ books }, null, 2))
+    await Fs.writeFile(DB_PATH, JSON.stringify({
+        books
+    }, null, 2))
 }
 
 main()
